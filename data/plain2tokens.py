b4485844a3b94ee40ebdb9ec46b9cdaa83d8e4a6
SebaOgas <saoga07.sao@gmail.com>
2025-06-01 15:00:05 -0300

    Tokenizador creado
    Reglas especiales básicas para tokenizar conceptos de la carrera creadas, se necesitan más.

---
+++ data/plain2tokens.py
@@ -0,0 +1,35 @@
+import spacy
+from spacy.symbols import ORTH
+from spacy.symbols import NORM
+import os
+import re
+
+rules_path = "./data/rules.txt"
+tokens_path = "./data/tokens.txt"
+plain_path = "./data/plain.txt"
+
+if os.path.exists(tokens_path):
+    os.remove(tokens_path)
+
+# Descargar el modelo con: python -m spacy download es_core_news_sm
+
+esp = spacy.load("es_core_news_sm")
+
+# Cargar las reglas especiales: secuencias de texto que no deben ser separadas
+
+with open(rules_path, "rb") as rf:
+    rules_txt = rf.read().decode("utf-8")
+    rules = [r.strip() for r in re.split(r'(?:\r\n)+', rules_txt)]
+    for r in rules:
+        esp.tokenizer.add_special_case(r, [{ORTH: r}])
+
+# Leer el archivo plano, generar los tokens y escribirlos al archivo de tokens (un token por línea)
+
+with open(plain_path, "rb") as pf:
+    txt = pf.read().decode("utf-8")
+    tokens = esp.tokenizer(txt)
+    with open(tokens_path, "wb") as tf:
+        for t in tokens:
+            if (t.text.strip()):
+                tf.write((t.text + "\n").encode("utf-8"))
+            
