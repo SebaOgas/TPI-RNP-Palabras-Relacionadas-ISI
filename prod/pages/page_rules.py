d760baa1886f8e5c7231053be727e0966641ab07
nombreusergithub <hsinyulin2971@gmail.com>
2025-06-23 23:44:48 -0300

    se añade nueva forma de juego

--- prod/pages/page_rules.py
+++ prod/pages/page_rules.py
@@ -6,17 +6,17 @@ st.title("ⓘ Reglas")
 st.markdown("""
 **ISINet Quizz** es un juego basado en la ISINet, que consiste en relacionar conceptos sobre temas de Ingeniería de Sistemas de Información. 
 
-### Ronda
-Se presenta un concepto a fin y **el jugador deberá ingresar conceptos relacionados**. Para pasar a la siguiente ronda, el concepto
-relacionado deberá estar en el **top 10 de conceptos más relacionados al propuesto**.
+### 🎯 Ronda
+Se presenta un concepto base y **el jugador deberá seleccionar los conceptos que considera que están más relacionados con el concepto base**. 
+Mientras más cerca esté el concepto dentro del top 10 de las palabras más relacionadas, **más puntos obtendrá**.
 
-### Vidas
-El juego es individual y **el jugador posee de 3 vidas**. Por cada intento fallido (el concepto ingresado no pertenece al top 10) pierde una vida.
+### ❤️ Vidas
+El juego es individual y **el jugador posee de 3 vidas**. Por cada intento fallido (el concepto seleccionado no pertenece al top 10) pierde una vida.
 El jugador pierde el juego una vez perdida sus 3 vidas, por lo que deberá iniciar uno nuevo.
             
-### Puntuación
-Cuando el jugador acierta entre uno de los 10 conceptos más relacionados, pasa a la siguiente ronda y consigue sumar puntos. El cálculo
-del puntaje procede según la posición en el top que ocupe el concepto ingresado, siendo la **puntuación máxima de 10 para el top 1** y la **mínima 1 para el top 10**.
+### 🏆 Puntuación
+Cuando el jugador selecciona entre uno de los 10 conceptos más relacionados consigue sumar puntos. El cálculo
+del puntaje procede según la posición en el top que ocupe el concepto ingresado, siendo la **puntuación máxima de 10 para el top 1** y la **mínima de 1 para el top 10**.
 """)
 
 
